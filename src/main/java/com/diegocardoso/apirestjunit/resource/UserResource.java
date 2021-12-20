c75a79d7b664ef28d34d126a5de34c3dd935d530
DiegoCardosoDev <diegocardososacramento>
2021-12-20 19:35:57 -0300

    class UserResource implemented

--- src/main/java/com/diegocardoso/apirestjunit/resource/UserResource.java
+++ src/main/java/com/diegocardoso/apirestjunit/resource/UserResource.java
@@ -2,6 +2,8 @@ package com.diegocardoso.apirestjunit.resource;
 
 
 import com.diegocardoso.apirestjunit.domain.User;
+import com.diegocardoso.apirestjunit.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,9 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findByid(@PathVariable Integer id) {
-        return ResponseEntity.ok().body(new User(1, "diego", "diego@cardoso", "123"));
+        return ResponseEntity.ok().body(service.findById(id));
+
 
     }
 
