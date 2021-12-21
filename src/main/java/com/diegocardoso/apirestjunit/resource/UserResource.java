1c2baa9a39852492cdeadfeb6f0f176f0d2d0eae
DiegoCardosoDev <diegocardososacramento>
2021-12-21 08:10:15 -0300

    DTO implemented

--- src/main/java/com/diegocardoso/apirestjunit/resource/UserResource.java
+++ src/main/java/com/diegocardoso/apirestjunit/resource/UserResource.java
@@ -2,7 +2,9 @@ package com.diegocardoso.apirestjunit.resource;
 
 
 import com.diegocardoso.apirestjunit.domain.User;
+import com.diegocardoso.apirestjunit.domain.userDTO.UserDTO;
 import com.diegocardoso.apirestjunit.services.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,11 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserResource {
 
     @Autowired
+    private ModelMapper mapper;
+
+    @Autowired
     private UserService service;
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<User> findByid(@PathVariable Integer id) {
-        return ResponseEntity.ok().body(service.findById(id));
+    public ResponseEntity<UserDTO> findByid(@PathVariable Integer id) {
+        return ResponseEntity.ok().body(mapper.map(service.findById(id), UserDTO.class));
 
 
     }
